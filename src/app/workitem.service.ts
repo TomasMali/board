602c676021e8c75c38812be5f65e05d6b21e30c5
TomasMali <megatoys92@gmail.com>
2020-03-14 21:27:16 +0100

    ciao

--- src/app/workitem.service.ts
+++ src/app/workitem.service.ts
@@ -8,9 +8,9 @@ import { Workitem } from './wokitem/workitem';
 export class WorkitemService {
   data: object
   loading: boolean
-   board = "https://93.49.6.246:3002/board"
-   insertWi = "https://93.49.6.246:3002/board/insert"
-   deleteWi = "https://93.49.6.246:3002/board/delete_one"
+   board = "https://93.49.6.246:3004/board"
+   insertWi = "https://93.49.6.246:3004/board/insert"
+   deleteWi = "https://93.49.6.246:3004/board/delete_one"
 
   constructor(public http: HttpClient) { }
 
